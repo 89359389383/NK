8088d5fd3fdc34ebed35c1fd07fc9ad3d1559d6e
NK <nba20093089@gmail.com>
2023-04-28 12:23:10 +0000

    “routes.rb”

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
-  :registrations => 'users/registrations',
-  :sessions => 'users/sessions',
-  :passwords => 'users/passwords',
-  :confirmations => 'users/confirmations',
-  :unlocks => 'users/unlocks',
-}
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks',
+  }
 
-devise_scope :user do
-  root :to => "users/sessions#new"
-  get "signup", :to => "users/registrations#new"
-  get "verify", :to => "users/registrations#verify"
-  get "login", :to => "users/sessions#new"
-  delete "logout", :to => "users/sessions#destroy"
-end
-  resources :products do
-    resources :reviews, only: [:create]
+  devise_scope :user do
+    root :to => "users/sessions#new"
+    get "signup", :to => "users/registrations#new"
+    get "verify", :to => "users/registrations#verify"
+    get "login", :to => "users/sessions#new"
+    delete "logout", :to => "users/sessions#destroy"
   end
+  resources :products
 end
